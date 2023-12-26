aba9056326f03f8209ef52b68e80332eb5873895
n777sam <n777sam@gmail.com>
2023-12-27 00:01:14 +0400

    task5

---
+++ task4/modules/constants.js
@@ -0,0 +1,39 @@
+
+export const SERVER_URL = "/cards";
+export const SERVER_URL_AUTHOR = "/author";
+
+export const POST = "POST";
+export const DELETE = "DELETE";
+export const PUT = "PUT";
+
+export const UPDATE = "UPDATE";
+export const REMOVE = "REMOVE";
+
+export const DEFAILT_VALUE = [
+  {
+    productName: "Тест 1",
+    hrefToIMG:
+      "https://media.istockphoto.com/id/1062947134/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D0%BE%D0%B1%D0%BE%D1%80-%D0%B2%D0%B0%D1%81%D0%B8%D0%BB%D0%B8%D1%8F-%D0%B1%D0%BB%D0%B0%D0%B6%D0%B5%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE-%D0%B8-%D0%B7%D0%BE%D0%BB%D0%BE%D1%82%D1%8B%D0%B5-%D0%BF%D0%B5%D1%80%D0%B2%D1%8B%D0%B5-%D0%BB%D1%83%D1%87%D0%B8-%D1%81%D0%BE%D0%BB%D0%BD%D1%86%D0%B0.jpg?s=612x612&w=0&k=20&c=rSRPoCl78qvsYxaRhOa0xS_z10jDDnJvhteWLM5w8yo=",
+    description: "Собор Василия Блаженного и золотые первые лучи солнца\n",
+    provider: "Поставщик 1",
+    id: "0",
+  },
+  {
+    productName: "Электросамокат Xiaomi Mijia Electric Scooter Pro",
+    hrefToIMG:
+      "https://mi29.ru/image/cache/cache/1001-2000/1387/main/7dd9-99867-0-1-800x800.jpg",
+    description:
+      "Mijia Pro от компании Xiaomi - это модернизированный вариант популярного, всемирно признанного электросамоката.",
+    provider: "Xiaomi ",
+    id: "1",
+  },
+  {
+    productName: "Телефон Apple iPhone 14 128Gb Midnight",
+    hrefToIMG:
+      "https://goodcom.ru/loaded/catalog/goods/thumbs/02449851a3eb11acc56c256af0cd7366.jpg",
+    description:
+      "Телефон Apple iPhone 14 работает под управление операционной системы iOS со стартовой версии 16.",
+    provider: "Хорошая связь",
+    id: "2",
+  },
+];
